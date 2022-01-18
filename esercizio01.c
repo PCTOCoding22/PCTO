dc8e9980222896dc6f22d95bdcb9007a7de4e0a8
Mr.Comodino <retest@lutyu.com>
2022-01-18 16:47:38 +0100

    ao

--- esercizio01.c
+++ esercizio01.c
@@ -3,18 +3,24 @@
 int main()
 {
     int eta;
- printf("Inserisci la tua età");
+ printf("Inserisci la tua età: ");
     scanf("%d", &eta);
     while(eta<0){
-        printf("Età non valida, inserire la tua età reale");
+        printf("Età non valida, inserire la tua età reale: ");
         scanf("%d", &eta);
     }
     if(eta >= 18){
+        if(eta > 170){
+        printf("Sei Umano, comunque maggiorenne\n");
+    }
+    else{
         printf("Sei maggiorenne\n");
+    }
     }
     else{
         printf("Sei minorenne\n");
     }
+   
 
 
     return(0);
