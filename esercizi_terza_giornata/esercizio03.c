e794757e1607b3fe3edbf143b36e5e66d797821b
S.Diacetti <S.Diacetti>
2022-01-19 12:09:08 +0100

    consegna esercizi terza giornata

--- esercizi_terza_giornata/esercizio03.c
+++ esercizi_terza_giornata/esercizio03.c
@@ -4,8 +4,17 @@ int main()
 {
     int x;
     int y;
-    scanf("%d", &x)
-    scanf("%d", &y)
+    printf("inserisci x: ");
+    scanf("%d", &x);
+    printf("inserisci y: ");
+    scanf("%d", &y);
     
-    if(x = y + 2)
+    if(x % y == 0) {
+        printf("y è un multiplo di x\n");
+    }
+    else {
+        printf("y non è un multiplo di x\n");
+    }
+
+    return 0;
 }
