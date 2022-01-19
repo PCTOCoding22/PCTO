7d024a7bb2bbe39babae00e7e51bda10ad5886a4
matilde <ciaomati@gmail.com>
2022-01-19 14:37:01 +0100

    ese05.c

---
+++ eseterzagiornata/ese05.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int x;
+    printf("inserire primo numero: ");
+    scanf("%d", &x);
+    int y;
+    printf("inserire secondo numero: ");
+    scanf("%d", &y);
+    int z;
+    printf("inserire il terzo numero: ");
+    scanf("%d", &z);
+    if(y-x==z-y)
+    {
+        printf("è una progressione aritmetica");
+    }
+    else
+    {
+        printf("non è una progressione aritmetica");
+    }
+    return 0;
+}
