6fb7b4d250246c35c4d0c21bddfa9a3cb6bf7d8e
S.Diacetti <S.Diacetti>
2022-01-19 15:19:37 +0100

    consegna esercizi terza giornata

---
+++ esercizi_terza_giornata/esercizio.collettivo.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main ()
+
+{
+
+    int x;
+    printf("scrivi un numero: ");
+    scanf("%d", &x);
+
+    int contatore = x - 1;
+
+    while(contatore > 1) {
+
+        if(x % contatore != 0) {
+
+            contatore = contatore - 1;
+
+        }
+        else {
+
+            printf("il numero non è primo \n");
+
+            return 0;
+
+        }
+
+    }
+
+    printf("il numero è primo \n");
+
+    return 0;
+
+}
