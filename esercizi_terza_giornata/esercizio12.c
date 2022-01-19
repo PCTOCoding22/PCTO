0256f7be103e3118433de3c2c13cd4d8cd0721b9
O.Benincasa <ciao@gmail.com>
2022-01-19 15:19:44 +0100

    consegna_esercizio12

---
+++ esercizi_terza_giornata/esercizio12.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main()
+{
+    int x;
+    printf("scrivi un numero");
+    scanf("%d", &x);
+    int count = x - 1 ;
+    while(count > 1)
+    {
+        if(x % count !=0){
+        count --;
+    } 
+    else
+    {
+        printf( "il numero non è primo");
+       return 0;
+    }
+    
+    }
+   printf("il numero è primo");
+
+   return 0;       
+}
