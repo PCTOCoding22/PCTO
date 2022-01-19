39d8729c7516e46d01cf049e5232b2804101a95b
Filippo <filippo.sandri97@gmail.com>
2022-01-19 12:49:04 +0100

    esercizio08

---
+++ consegnaeserciziterzagiornata/esercizio08.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(){
+    int x;
+    int y;
+    int z;
+    printf("inserisci il primo numero:");
+    scanf("%d", &x);
+    printf("inserisci il primo numero:");
+    scanf("%d", &y);
+    printf("inserisci il primo numero:");
+    scanf("%d", &z);
+    if (x < 0 || y < 0 || z < 0){
+        printf("ERROR");
+        return 0;
+    }
+    if( x == y && y == z && z == x){
+        printf("il triangolo è equilatero\n");
+    }
+    if( x == y || y == z || z == x){
+        printf("il triangolo è isoscele\n");
+    }
+    else{
+        printf("il triangolo è scaleno\n");
+    }
+    return 0;
+    }
