6678379d3bd5d1180d6a0ec2a11b179d5437390d
lol69 <test@spex.com>
2022-01-19 11:47:17 +0100

    ok

--- random_number.c
+++ random_number.c
@@ -10,33 +10,42 @@ int main()
     printf("-----------Inserisci un numero-----------\n");
 
 
-    printf("------------------------------vite :%d---\n", numero_vite);
+    printf("------------------------------Euro: %d---\n", numero_vite);
 
     int numero_inserito;
     int numero_casuale = rand() % 10 + 1;
 
-    scanf("%d", &numero_inserito );
-
-    printf("-----------Il numero è %d-----------\n", numero_casuale);
     
-    if( numero_vite > 0)
+
+    
+    
+    while( numero_vite > 0)
     {
+        int numero_casuale = rand() % 10 + 1;
+        
+        scanf("%d", &numero_inserito );
+        printf("Il numero è: (%d)\n", numero_casuale);
     
 
     if (numero_inserito == numero_casuale)
     {
+        numero_vite += 5;
         
         printf("Bravo %d è uguale a %d!\n", numero_inserito, numero_casuale);
+        printf("Hai vinto ben 3 euro\n");
+        printf("------------------------------Euro: %d---\n", numero_vite);
     }
     else{
         numero_vite += -1;
         printf("Mi dispiace ma %d non è uguale a %d :((\n", numero_inserito, numero_casuale);
+        printf("Hai perso solo 1 euro\n\n");
+        printf("------------------------------Euro: %d---\n", numero_vite);
     }
     }
-    else{
     
-      printf("Hai esaurito le vite");
-    }
+    
+      printf("Hai esaurito i soldi\n");
+    
 
     
 
