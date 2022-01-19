0811de7cfcd648d0ce1fa9228573f208a22b44c2
sofia <test@spacex.com>
2022-01-19 14:26:40 +0100

    messaggio

---
+++ consegnaeserciziterzagiornata/esercizio04.c
@@ -0,0 +1,18 @@
+#include  <stdio.h>
+
+int main() //inizio
+
+int temperatura
+printf("inserisci temperatura in celsius");
+scanf("%d", &);
+
+if (temperatura < -273,15)
+{
+    printf("errore temperatura inferiore a 0");
+}
+else
+{
+    printf("temperatura in fahrenheit");
+}
+result 0;
+}
