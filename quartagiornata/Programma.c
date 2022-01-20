149479e248473ba8bb861ab921183831dafc1867
S.Berlusca <test@spacex.com>
2022-01-20 11:03:00 +0100

    esercizi

---
+++ quartagiornata/Programma.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int x; //voti
+    int y; //punti totali
+    printf("Inserire voto\n");
+    scanf("%d", &x);
+
+     if(x<=5)
+     {
+       y=x;
+     }
+     if(x==6)
+     {
+       y=x+2;
+     }
+     printf("")
+ return 0;
+
+}
