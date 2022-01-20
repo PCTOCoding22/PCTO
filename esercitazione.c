81693f87e0a770c5f22ceac5e6c6724a3c84bdb7
O.Benincasa <ciao@gmail.com>
2022-01-20 11:53:51 +0100

    oawkdoiakjwd

---
+++ esercitazione.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() 
+{
+    for (int x = 1 ; x <= 12; x = x + 12) 
+    {
+        for (int y = x; y <= 12; y = y + 12) 
+    }  
+    }
+    for ( int z = x; z = <=12; z = z + 12){
+        printf("x%d y%d z%d\n", x,y,z);
+    }
+
+      return 0;
+}
+
+
